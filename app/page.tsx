a41a07d1044ea17c1ed1c1636ff4f3ab07fc3a9d
yusufproject01 <yusufproject01@gmail.com>
2024-07-24 21:01:27 +0700

    by yusuf

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div className="w-full h-screen">
       <h1>home</h1>
+      <Link href="/profile">Profile</Link>
     </div>
   );
 }
---
+++ app/profile/page.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link"
+
+const page = () => {
+    return (
+        <div>
+            <h1>Profile</h1>
+            <Link href="/">Home</Link>
+        </div>
+    )
+}
+
+export default page
